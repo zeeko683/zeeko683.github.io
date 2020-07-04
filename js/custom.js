223b6b5f0e277914bed6868f73422aeff9ccd896
zeeko683 <harmon@hotmail.nl>
2020-07-04 22:58:12 +0200

    epic
    added fancy scrollbar

--- js/custom.js
+++ js/custom.js
@@ -110,3 +110,12 @@ function initComparisons() {
   }
 }
 
+
+// scrollbalk kutjes whoohooo
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function(){
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = progressHeight + "%";
+}
